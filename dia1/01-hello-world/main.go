1da8fd8c7d6908388e4558fddbd162c7054822d2
Manoel Domingues Junior <mdjunior@ufrj.br>
2020-02-23 20:42:34 -0300

    [FEAT] Add more info about Hello World

--- dia1/01-hello-world/main.go
+++ dia1/01-hello-world/main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+// Todo curso de programação começa com o Hello World!
+// Nesse exemplo podemos ver alguns aspectos iniciais de um programa em Go.
+// - O primeiro é a declaração `package` no início do arquivo.
+// - O segundo é a declaração `import` utilizada para carregar outras bibliotecas.
+// - O terceiro é a função principal `main`.
+
+// Referência sobre Hello, World: https://blog.hackerrank.com/the-history-of-hello-world/
+
 func main() {
 	fmt.Println("Hello world!")
 }
